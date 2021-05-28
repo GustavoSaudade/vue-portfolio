5308cc1e88464be6bbe87bd514201ee86f5511b6
GustavoSaudade <gustavosaudade@hotmail.com>
2021-05-28 19:38:58 -0300

    Added more circle progress bar

--- src/components/skills-bar.vue
+++ src/components/skills-bar.vue
@@ -1,21 +1,79 @@
 <template>
   <div>
-    <h1 class="subtilte">Skills</h1>
-    <ul>
-      <li class="skill" v-for="(skill, i) in skillSet" :key="i">
-        <div :style="{ width: skill.initLevel + '%' }">
-          <label>{{ skill.area }}</label>
-          <label>{{ skill.initLevel + "%" }}</label>
-        </div>
-      </li>
-    </ul>
+    <div class="row">
+      <div class="col-12">
+        <h1 class="subtilte">Skills</h1>
+        <ul>
+          <li class="skill" v-for="(skill, i) in skillSet" :key="i">
+            <div :style="{ width: skill.initLevel + '%' }">
+              <label>{{ skill.area }}</label>
+              <label>{{ skill.initLevel + "%" }}</label>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-6">
+        <Progress :radius="50" :strokeWidth="10" value="86.12">
+          <template v-slot:footer>
+            <b>Bolder & Bigger One</b>
+          </template>
+        </Progress>
+      </div>
+      <div class="col-6">
+        <Progress
+          :transitionDuration="5000"
+          :radius="50"
+          :strokeWidth="10"
+          value="86.12">
+          <template v-slot:footer>
+            <b>Slow One</b>
+          </template>
+        </Progress>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-3">
+        <Progress strokeColor="#FF00AA" value="16.88">
+          <template v-slot:footer>
+            <b>More Color</b>
+          </template>
+        </Progress>
+      </div>
+      <div class="col-3">
+        <Progress strokeColor="#FF00AA" value="16.88">
+          <template v-slot:footer>
+            <b>More Color</b>
+          </template>
+        </Progress>
+      </div>
+      <div class="col-3">
+        <Progress strokeColor="#FF00AA" value="16.88">
+          <template v-slot:footer>
+            <b>More Color</b>
+          </template>
+        </Progress>
+      </div>
+      <div class="col-3">
+        <Progress strokeColor="#FF00AA" value="16.88">
+          <template v-slot:footer>
+            <b>More Color</b>
+          </template>
+        </Progress>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import Progress from 'easy-circular-progress'
+
 export default {
   name: 'SkillsBar',
-
+  components: {
+    Progress
+  },
   data () {
     return {
       intervalID: '',
